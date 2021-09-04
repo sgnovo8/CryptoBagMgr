909845aa266287c043202e75c4902b9f625d3545
sgnovo8 <88598099+sgnovo8@users.noreply.github.com>
2021-09-04 09:24:35 -0400

    Update CryptobagMgr.sol

--- CryptobagMgr/contracts/CryptobagMgr.sol
+++ CryptobagMgr/contracts/CryptobagMgr.sol
@@ -1,67 +1,68 @@
 pragma solidity >=0.8.0;
 
-contract Playlists {
-  uint public totalPlaylist;
-  uint public totalSong;
+contract CryptoBagMgr {
+  uint public totalCryptobag;
+  uint public totalCoin;
 
-  mapping(uint => Playlist) public playlists;
-  mapping(uint => Song) public songs;
+  mapping(uint => Cryptobag) public cryptobags;
+  mapping(uint => Coin) public coins;
 
   constructor(){
-    totalPlaylist = 0;
-    totalSong = 0;
+    totalCryptobag = 0;
+    totalCoin = 0;
   }
-  event PlaylistCreated(uint id, string name,address creator,uint songs);
-  event SongAdded(uint id,uint playlistId,string name,address creator );
+  event CryptobagCreated(uint id, string name, address creator, uint coins);
+  event CoinAdded(uint id,uint cryptocoinsId,string name,address creator );
 
-  struct Playlist{
+  struct Cryptobag{
     uint id;
     string name;
     address creator;
-    uint songs;
+    uint coins;
   }
   
-  struct Song{
+  struct Coin{
     uint id;
-    uint playlistId;
+    uint cryptobagId;
     string name;
     address creator;
   }
 
  
+ 
 
-  function createPlaylist(string memory _name) external {
+  function createCryptobag(string memory _name) external {
     require(bytes(_name).length > 0 , "Name can not be empty");
-    totalPlaylist += 1;
-    playlists[totalPlaylist] = Playlist(totalPlaylist,_name,msg.sender,0);
-    emit PlaylistCreated(totalPlaylist, _name, msg.sender, 0);
+    totalCryptobag += 1;
+    cryptobags[totalCryptobag] = Cryptobag(totalCryptobag,_name, msg.sender,0);
+    emit CryptobagCreated(totalCryptobag, _name, msg.sender, 0);
   
   }
 
-  function addSongToPlaylist(string memory _name, uint _playlistId)  external {  
-    require(_playlistId <= totalPlaylist,"Wrong playlist id");
-    totalSong += 1;
-    songs[totalSong] = Song(totalSong,_playlistId,_name,msg.sender);
-    playlists[_playlistId].songs += 1;
+  function addCoinToCryptobag(string memory _name, uint _cryptobagId)  external {  
+    require(_cryptobagId <= totalCryptobag,"Wrong cryptobag id");
+    totalCoin += 1;
+    coins[totalCoin] = Coin(totalCoin,_cryptobagId,_name,msg.sender);
+    cryptobags[_cryptobagId].coins += 1;
 
-    emit SongAdded(totalSong, _playlistId, _name,msg.sender);
+    emit CoinAdded(totalCoin, _cryptobagId, _name,msg.sender);
 
   }
 
-  function getPlaylistSongs(uint _playlistId) external view returns(Song[] memory){
-    require(_playlistId <= totalPlaylist,"Wrong playlist id");
-    uint songSize = playlists[_playlistId].songs;
+  function getCryptobagCoins(uint _cryptobagId) external view returns(Coin[] memory){
+    require(_cryptobagId <= totalCryptobag,"Wrong cryptobag id");
+    uint coinAmt = cryptobags[_cryptobagId].coins;
     
-    Song[] memory _songs = new Song[](songSize);
+   Coin[] memory _coins = new Coin[](coinAmt);
 
     uint currentIndex = 0;
-    for(uint i=1;i<=totalSong;i++){
-      if(songs[i].playlistId == _playlistId){
-        _songs[currentIndex] = songs[i];
+    for(uint i=1;i<=totalCoin;i++){
+      if(coins[i].cryptobagId == _cryptobagId){
+        _coins[currentIndex] = coins[i];
         currentIndex += 1;
       }
     }
-    return _songs;
+    return _coins;
   }
 
 }
